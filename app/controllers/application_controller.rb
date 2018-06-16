15611515bf859cec678cff2e3e6921a1ca698522
Alex Wilson <fonzmp@gmail.com>
2018-06-16 20:26:10 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,6 +13,7 @@ class ApplicationController < Sinatra::Base
   post '/login' do
     @user = User.find_by(username: params[:username], password: params[:password])
     session[:user_id] = @user.id
+    binding.pry
 
     redirect '/account'
   end
